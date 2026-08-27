ccb2dda1fe9c48c82bcd35204634d50e814eac5c
kames b <kamesh.vnv0503@gmail.com>
2026-08-27 19:18:21 +0000

    added new files

--- models/silver/dim_silver_customers_historical.sql
+++ models/silver/dim_silver_customers_historical.sql
@@ -4,7 +4,7 @@ select
     segment,
     country,
     state,
-    dbt_start_ts as valid_from,
-    coalesce(dbt_end_ts, '9999-12-31'::timestamp) as valid_to,
-    case when dbt_end_ts is null then true else false end as is_current_record
+    dbt_valid_from,
+    coalesce(dbt_valid_to, '9999-12-31'::timestamp) as dbt_valid_to,
+    case when dbt_valid_to is null then true else false end as is_current_record
 from {{ ref('scd_customers') }}
